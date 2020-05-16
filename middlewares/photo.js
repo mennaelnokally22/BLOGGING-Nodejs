aa90d637657c503150ce2e9d6981e4bae47cb4f4
Menna <menna.elnokally@gmail.com>
2020-05-16 07:13:32 +0200

    🔥 REMOVE: Multer storage

--- middlewares/photo.js
+++ middlewares/photo.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 module.exports = multer({
   storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, '../public/uploads'));
-    },
-    filename: function (req, file, cb) {
-      cb(null, `${Date.now()}${path.extname(file.originalname)}`);
-    },
+    // destination: function (req, file, cb) {
+    //   cb(null, path.join(__dirname, '../public/uploads'));
+    // },
+    // filename: function (req, file, cb) {
+    //   cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+    // },
   }),
   fileFilter(req, file, cb) {
     const isPhoto = file.mimetype.startsWith('image/');
